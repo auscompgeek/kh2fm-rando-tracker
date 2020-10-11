a9c8ca8afd01cf9878f95ed4a1bd84d318baadef
auscompgeek <auscompgeek@users.noreply.github.com>
2020-10-11 05:19:05 +0000

    Deploying to gh-pages from  @ 5c86e842198100cb4dba504d01375507cb2c9c2c 🚀

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,3 @@
-function setTheme(){document.documentElement.setAttribute("theme",theme),localStorage.theme=theme}var theme=localStorage.theme;void 0===theme&&(theme="dark"),"dark"===theme?setTheme():theme="light";
+"use strict";function setTheme(){document.documentElement.setAttribute("theme",theme);try{localStorage.theme=theme}catch(a){}}try{var theme=localStorage.theme}catch(a){console.error("Could not read localStorage, using defaults")}theme===void 0&&(theme="dark"),"dark"===theme?setTheme():theme="light";
+
+//# sourceMappingURL=theme.js.map
